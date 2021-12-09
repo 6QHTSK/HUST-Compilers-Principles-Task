1b41d6f5162941af03c5178bbbbad98564b861b9
6QHTSK <psk2019@qq.com>
2021-12-09 22:42:43 +0800

    更新Readme.md

--- MiniCGrammar/T4/test.c
+++ MiniCGrammar/T4/test.c
@@ -1,20 +1,9 @@
-int read(){
-	int a=0;
-	a = getchar();
-	return a - 48;
-}
 int main(){
-	int m,n;
-	int i=48;
-	m = read();
-	n = read();
-    if(m == n ){
-        putchar(i);
-    }else{
-        i = i + 1;
-        putchar(i);
-    }
-    i = i + 1;
-    putchar(i);
-	return 0;
+    int b;
+    int a,c;
+    int d = 0;
+    b = 0;
+    c = 2;
+    a = b + c;
+    return a;
 }
--- readme.md
+++ readme.md
@@ -1,7 +1,23 @@
-## Flex & Bison 入门
+## [Flex & Bison 入门](/)
 
 华中科技大学2021年编译原理实验1任务记录
 
-## MiniC词法\(Flex\)
+## [MiniC词法](/MiniCTask)
 
 华中科技大学2021年编译原理实验2任务记录
+
+## [MiniC语法（生成语法树）](/MiniCGrammar)
+
+华中科技大学2021年编译原理实验3任务记录
+
+## [LLVM生成IR](/LLVM)
+
+华中科技大学2021年编译原理实验4任务记录
+
+[详细题解](/LLVM)
+
+## Hint
+
+获得评测脚本，避免摸索的方法：
+
+![图片](/assets/screen_shot1.png)
